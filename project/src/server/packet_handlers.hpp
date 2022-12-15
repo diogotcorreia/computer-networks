f3d63fd3a3a5aeaffc8288a54f3b25a2c6a37339
trvds <tiagovs02@gmail.com>
2022-12-15 15:57:57 +0000

    Implement reveal action on server

--- project/src/server/packet_handlers.hpp
+++ project/src/server/packet_handlers.hpp
@@ -14,4 +14,7 @@ void handle_guess_letter(std::stringstream &buffer, Address &addr_from,
 void handle_guess_word(std::stringstream &buffer, Address &addr_from,
                        GameServerState &state);
 
+void handle_reveal_word(std::stringstream &buffer, Address &addr_from,
+                        GameServerState &state);
+
 #endif
