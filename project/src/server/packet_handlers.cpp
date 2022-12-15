5109762646e184397e83f91beddc4d9dc1064f50
trvds <tiagovs02@gmail.com>
2022-12-15 14:28:46 +0000

    Implement quit action on server

--- project/src/server/packet_handlers.cpp
+++ project/src/server/packet_handlers.cpp
@@ -112,3 +112,29 @@ void handle_guess_word(std::stringstream &buffer, Address &addr_from,
   send_packet(response, addr_from.socket, (struct sockaddr *)&addr_from.addr,
               addr_from.size);
 }
+
+void handle_quit_game(std::stringstream &buffer, Address &addr_from,
+                      GameServerState &state) {
+  QuitGameServerbound packet;
+  packet.deserialize(buffer);
+
+  state.cdebug << "Received request to quit game from player '"
+               << packet.player_id << "'" << std::endl;
+
+  QuitGameClientbound response;
+  try {
+    ServerGame &game = state.getGame(packet.player_id);
+    game.finishGame();
+    response.success = true;
+  } catch (NoGameFoundException &e) {
+    response.success = false;
+  } catch (std::exception &e) {
+    std::cerr << "There was an unhandled exception that prevented the server "
+                 "from handling a quit game request:"
+              << e.what() << std::endl;
+    return;
+  }
+
+  send_packet(response, addr_from.socket, (struct sockaddr *)&addr_from.addr,
+              addr_from.size);
+}
