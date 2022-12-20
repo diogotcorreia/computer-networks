7fd568f39428e658ffc86052b73df133baaab34b
Diogo Correia <me@diogotc.com>
2022-12-20 13:42:04 +0000

    Use switch instead of if

--- project/src/client/commands.cpp
+++ project/src/client/commands.cpp
@@ -96,21 +96,24 @@ void StartCommand::handle(std::string args, PlayerState& state) {
   state.sendUdpPacketAndWaitForReply(packet_out, rsg);
 
   // Check status
-  if (rsg.status == ReplyStartGameClientbound::status::OK) {
-    // Start game
-    ClientGame* game = new ClientGame(player_id, rsg.n_letters, rsg.max_errors);
-    state.startGame(game);
-    // Output game info
-    std::cout << "Game started successfully!" << std::endl;
-  } else if (rsg.status == ReplyStartGameClientbound::status::NOK) {
-    std::cout
-        << "Game failed to start: that player already has an on-going game."
-        << std::endl;
-  } else if (rsg.status == ReplyStartGameClientbound::status::ERR) {
-    std::cout << "Game failed to start: packet was wrongly structured."
-              << std::endl;
-  } else {
-    std::cout << "Game failed to start: unknown error." << std::endl;
+  switch (rsg.status) {
+    case ReplyStartGameClientbound::status::OK:
+      // Start game
+      ClientGame* game =
+          new ClientGame(player_id, rsg.n_letters, rsg.max_errors);
+      state.startGame(game);
+      // Output game info
+      std::cout << "Game started successfully!" << std::endl;
+      break;
+    case ReplyStartGameClientbound::status::NOK:
+      std::cout
+          << "Game failed to start: that player already has an on-going game."
+          << std::endl;
+      break;
+    case ReplyStartGameClientbound::status::ERR:
+      std::cout << "Game failed to start: packet was wrongly structured."
+                << std::endl;
+      break;
   }
 }
 
