4db776c75e513fb08d1f317f6d9653bb0b2d77fa
Diogo Correia <me@diogotc.com>
2022-12-20 16:56:09 +0000

    Fix various issues related to trial numbers on packets (#87)

--- project/src/server/packet_handlers.cpp
+++ project/src/server/packet_handlers.cpp
@@ -39,17 +39,19 @@ void handle_start_game(std::stringstream &buffer, Address &addr_from,
 void handle_guess_letter(std::stringstream &buffer, Address &addr_from,
                          GameServerState &state) {
   GuessLetterServerbound packet;
-  packet.deserialize(buffer);
-
-  state.cdebug << "Received request to guess letter '" << packet.guess
-               << "' from player '" << packet.player_id << "'" << std::endl;
-
   GuessLetterClientbound response;
   try {
+    packet.deserialize(buffer);
+
+    state.cdebug << "Received request to guess letter '" << packet.guess
+                 << "' from player '" << packet.player_id << "'" << std::endl;
+
     ServerGameSync game = state.getGame(packet.player_id);
 
     response.trial = game->getCurrentTrial();
     auto found = game->guessLetter(packet.guess, packet.trial);
+    // Must set trial again in case of replays
+    response.trial = game->getCurrentTrial() - 1;
 
     if (game->hasLost()) {
       response.status = GuessLetterClientbound::status::OVR;
@@ -68,7 +70,11 @@ void handle_guess_letter(std::stringstream &buffer, Address &addr_from,
     response.trial -= 1;
   } catch (InvalidTrialException &e) {
     response.status = GuessLetterClientbound::status::INV;
+    response.trial -= 1;
   } catch (GameHasEndedException &e) {
+    response.status = GuessLetterClientbound::status::ERR;
+  } catch (InvalidPacketException &e) {
+    state.cdebug << "Invalid packet" << std::endl;
     response.status = GuessLetterClientbound::status::ERR;
   } catch (std::exception &e) {
     std::cerr << "There was an unhandled exception that prevented the server "
@@ -84,17 +90,19 @@ void handle_guess_letter(std::stringstream &buffer, Address &addr_from,
 void handle_guess_word(std::stringstream &buffer, Address &addr_from,
                        GameServerState &state) {
   GuessWordServerbound packet;
-  packet.deserialize(buffer);
-
-  state.cdebug << "Received request to guess word '" << packet.guess
-               << "' from player '" << packet.player_id << "'" << std::endl;
-
   GuessWordClientbound response;
   try {
+    packet.deserialize(buffer);
+
+    state.cdebug << "Received request to guess word '" << packet.guess
+                 << "' from player '" << packet.player_id << "'" << std::endl;
+
     ServerGameSync game = state.getGame(packet.player_id);
 
     response.trial = game->getCurrentTrial();
     bool correct = game->guessWord(packet.guess, packet.trial);
+    // Must set trial again in case of replays
+    response.trial = game->getCurrentTrial() - 1;
 
     if (game->hasLost()) {
       response.status = GuessWordClientbound::status::OVR;
@@ -110,7 +118,11 @@ void handle_guess_word(std::stringstream &buffer, Address &addr_from,
     response.trial -= 1;
   } catch (InvalidTrialException &e) {
     response.status = GuessWordClientbound::status::INV;
+    response.trial -= 1;
   } catch (GameHasEndedException &e) {
+    response.status = GuessWordClientbound::status::ERR;
+  } catch (InvalidPacketException &e) {
+    state.cdebug << "Invalid packet" << std::endl;
     response.status = GuessWordClientbound::status::ERR;
   } catch (std::exception &e) {
     std::cerr << "There was an unhandled exception that prevented the server "
